a27632e2399769aa4780a36888f9d3cba87971bd
j-norwood-young <jason@10layer.com>
2012-08-15 10:34:19 +0200

    Keep user logged in forever

--- application/config/config.php
+++ application/config/config.php
@@ -245,7 +245,7 @@ $config['encryption_key'] = 'DFSJKLisa98uSD(^&hfn^55J';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
