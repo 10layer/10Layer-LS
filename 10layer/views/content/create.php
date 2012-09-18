60df295aad22469fe98fec9c542812dbac6be0b5
j-norwood-young <jason@10layer.com>
2012-09-18 12:11:55 +0200

    New create and edit files not so hidden

---
+++ 10layer/views/content/create.php
@@ -0,0 +1,213 @@
+<?php 
+	$data["menu1_active"]="create";
+	$data["menu2_active"]="create/".$type;
+	$this->load->view('templates/header',$data);
+	link_js("/resources/js/forms.js");
+	ckeditor();
+?>
+
+<script src="/resources/js/davis.min.js"></script>
+<link rel="stylesheet" href="/resources/chosen/chosen.css">
+<script src="/resources/chosen/chosen.jquery.js"></script>
+
+<script language="javascript">
+	$(function() {
+		//Set the API key
+		$(document.body).data('api_key', '<?= $this->config->item('api_key') ?>');
+		
+		//Router
+		var app = Davis(function() {
+			this.configure(function () {
+				this.generateRequestOnPageLoad = true;
+				this.raiseErrors = true;
+				this.formSelector = "noforms";
+			});
+			
+			this.before('/create/:content_type', function(req) {
+				/*if ($(document.body).data('content_type') == req.params['content_type'] && $(document.body).data('page')=='list') {
+					return false;
+				}*/
+			});
+		
+			this.get('#', function(req) {});
+			this.get('/create/:content_type', function(req) {
+				$(document.body).data('content_type', req.params['content_type']);
+				$(document.body).data('page', 'list');
+				prepRouter();
+				init_create();
+			});
+		});
+		
+		app.start();
+		
+		function prepRouter() {
+			$('#dyncontent').children().find('.richedit').each(function() {
+				var name=$(this).attr('name');
+				var o=CKEDITOR.instances[name];
+			    if (o) o.destroy();
+			});
+		}
+		
+		function init_create() {
+			content_type=$(document.body).data('content_type');
+			$(".menuitem").each(function() {
+				$(this).removeClass('selected');
+			});
+			$('#menuitem_'+content_type).addClass('selected');
+			$('#dyncontent').html("Loading...");
+			$.getJSON("<?= base_url() ?>api/content/blank?jsoncallback=?", { api_key: $(document.body).data('api_key'), content_type: content_type, meta: true }, function(data) {
+				$('#dyncontent').html(_.template($("#create-template").html(), { data:data, content_type: content_type }));
+				init_form();
+			});
+		}
+		
+		$(document).on('click', '#dosubmit_right', function() {
+			if (!$(document.body).data('saving')) {
+				save();
+			}
+			return false;
+		});
+		
+		$(document).ajaxError(function(e, xhr, settings, exception) { 
+			//$("#dyncontent").html('<h1>Caught error</h1>'+xhr.responseText); 
+		});
+		
+		$("#dyncontent").ajaxComplete(function() {
+			//cl.hide();
+		});
+		
+		$("#dyncontent").ajaxStart(function() {
+			//cl.show();
+		});
+		
+		function save() {
+			for ( instance in CKEDITOR.instances )
+				CKEDITOR.instances[instance].updateElement();
+			content_type=$(document.body).data('content_type');
+			urlid=$(document.body).data('urlid');
+			if (!$(document.body).data('saving')) {
+				$(document.body).data('saving', true);
+				var formData = new FormData($('#contentform')[0]);
+				$.ajax({
+					url: "<?= base_url() ?>api/content/save?api_key=<?= $this->config->item('api_key') ?>&content_type="+content_type,  //server script to process data
+					type: 'POST',
+					xhr: function() {  // custom xhr
+					    myXhr = $.ajaxSettings.xhr();
+					    if(myXhr.upload){ // check if upload property exists
+					        myXhr.upload.addEventListener('progress',uploadProgress, false); // for handling the progress of the upload
+				    	}
+					    return myXhr;
+					},
+					//Ajax events
+					beforeSend: uploadBefore,
+					success: uploadComplete,
+					error: uploadFailed,
+					// Form data
+					data: formData,
+					//Options to tell JQuery not to process data or worry about content-type
+					cache: false,
+					contentType: false,
+					processData: false
+				});
+			}
+		}
+		
+		function uploadBefore(e) {}
+		
+		function uploadProgress(e) {
+			console.log("Upload progress");
+			console.log(e);
+		}
+		
+		function uploadComplete(data) {
+			$(document.body).data("saving",false);
+			if (data.error) {
+				$("#msgdialog-header").html("Error");
+				var info = (data.info) ? data.info : '';
+				if (_.isArray(info)) {
+					var tmp='';
+					for(var x=0; x<info.length; x++) {
+						tmp+="<li>"+info[x]+"</li>";
+					}
+				}
+				info="<ul>"+tmp+"</li>";
+				$("#msgdialog-body").html("<h4>"+data.msg+"</h4><p>"+info+"</p>");
+				$("#msgdialog-buttons").html('<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>');
+			    $("#msgdialog").modal();
+			} else {
+				$("#msgdialog-header").html("Saved");
+				$("#msgdialog-body").html("<p>Content has been successfully saved</p>");
+				$("#msgdialog-buttons").html("<a data-dismiss='modal' class='btn' href='<?= base_url() ?>create/"+$(document.body).data('content_type')+"'>Create another</a> <button class='btn' data-dismiss='modal' aria-hidden='true'>Reuse info</button> <a class='btn' href='<?= base_url() ?>edit/"+$(document.body).data('content_type')+"/"+data.id+"'>Edit</a>");
+				$("#msgdialog").modal();
+			}
+		}
+		
+		$("#dyncontent").delegate(".add-relation","click",function() {
+		//Creates the popup box for adding a new item
+			var fieldname=$(this).attr("contenttype")+"_"+$(this).attr("fieldname");
+			$("#createdialog").dialog({ minWidth: 700, modal: true, }).load(
+				"/create/fullview/"+$(this).attr("contenttype")+"/embed"
+			);
+			$("#createdialog").data("fieldname",fieldname);
+			return false;
+		});
+
+	});
+</script>
+
+<script type='text/template' id='create-template'>
+	<%	
+		if (typeof popup == 'undefined') {
+			popup = false;
+		}
+	%>
+	<div id="create-content" class="boxed wide">
+		<h2>Create - <%= content_type %></h2>
+		<form id='contentform' class='form-horizontal span12 <%= (popup) ? "popupform" : "contentform" %>' method='post' enctype='multipart/form-data' action='<?= base_url() ?>api/content/save?api_key=<%= $(document.body).data('api_key') %>'>
+		<input type='hidden' name='action' value='submit' />
+		<% _.each(data.meta, function(field) { %>
+			<% if (!field.hidden) { %>
+				<%= _.template($('#create-field-'+field.type).html(), { field: field, urlid: false, content_type: content_type  }) %>
+			<% } %>
+		<% }); %>
+		<% if (popup) { %>
+		<button contenttype="<%= content_type %>" fieldname="<%= name %>" class='dosubmit_popup'>Save</button>
+		<% } %>
+		</form>
+	</div>
+</script>
+
+<?php
+	$this->load->view("snippets/javascript_templates");
+?>
+<div class="navbar navbar-fixed-bottom">
+	<div class="navbar-inner">
+		<div class="container">
+			<ul class="nav">
+				<li><button class="btn btn-primary" id="dosubmit_right">Save</button></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="modal hide fade" id="msgdialog" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+		<h3 id="msgdialog-header"></h3>
+	</div>
+	<div class="modal-body" id="msgdialog-body">
+	</div>
+	<div class="modal-footer">
+		<div id="msgdialog-buttons" class="btn-group">
+		</div>
+	</div>
+</div>
+
+<div id="createdialog"></div>
+<div id="dyncontent">
+
+</div>
+<?php
+	$this->load->view("templates/footer");
+
+?>
