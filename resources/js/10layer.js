cce43aeb558c312caabf4618238f878d7cc2ace2
j-norwood-young <jason@10layer.com>
2012-11-15 09:28:00 +0200

    Move some js from templates file to 10layer.js

--- resources/js/10layer.js
+++ resources/js/10layer.js
@@ -5,3 +5,20 @@ function nullStr(s) {
 		return('');
 	}
 }
+
+function leadingZeros(s) {
+    if (s<10) {
+    	return '0'+s;
+    }
+    return s;
+}
+
+function sqlCurrentDate() {
+    var d = new Date();
+    return leadingZeros(d.getFullYear())+'-'+leadingZeros(d.getMonth()+1)+'-'+leadingZeros(d.getDate());
+}
+
+function sqlCurrentTime() {
+    var d = new Date();
+    return leadingZeros(sqlCurrentDate())+' '+leadingZeros(d.getHours())+':'+leadingZeros(d.getMinutes());
+}
