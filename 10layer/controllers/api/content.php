36fcb9dfe3383f0db21b20aba20d1a747041d52e
j-norwood-young <jason@10layer.com>
2012-11-20 17:13:40 +0200

    A supercalss for APIs

--- 10layer/controllers/api/content.php
+++ 10layer/controllers/api/content.php
@@ -1,16 +1,12 @@
 <?php
+	require_once('10layer/system/TL_Api.php');
+	
 	/**
 	 * Content class
 	 * 
 	 * @extends CI_Controller
 	 */
-	class Content extends CI_Controller {
-		
-		protected $secure=false;
-		protected $_render=true;
-		private $_start_time=0;
-		public $vars=array();
-		public $data;
+	class Content extends TL_Api {
 		
 		/**
 		 * __construct function.
@@ -19,21 +15,7 @@
 		 * @return void
 		 */
 		public function __construct() {
-			$this->_start_time=microtime(true);
 			parent::__construct();
-			$this->load->library("tlsecurity");
-			$this->tlsecurity->ignore_security();
-			$this->secure=$this->_check_secure();
-			if (!$this->secure) {
-				$this->mongo_db->where_gte("major_version", "4");
-			}
-			$this->data=array(
-				"error"=>false,
-				"timestamp"=>time(),
-				"msg"=>"",
-				"content"=>array()
-			);
-			$this->vars=array_merge($_GET, $_POST);
 			if (empty($this->vars)) {
 				$this->data["error"]=true;
 				$this->data["msg"]="No GET or POST variables";
@@ -444,46 +426,6 @@
 			}
 		}
 		
-		/**
-		 * _check_secure function.
-		 * 
-		 * Returns true if we've sent a valid API key, else false
-		 *
-		 * @access private
-		 * @return boolean
-		 */
-		private function _check_secure() {
-			$api_key=$this->input->get_post("api_key");
-			$api_key=trim($api_key);
-			if (empty($api_key)) {
-				return false;
-			}
-			$comp_api_key=$this->config->item('api_key');
-			if (empty($comp_api_key)) {
-				return false;
-			}
-			if ($comp_api_key==$api_key) {
-				return true;
-			}
-			return false;
-		}
-		
-		/**
-		 * returndata function.
-		 * 
-		 * If $this->_render is true, print the results as json
-		 *
-		 * @access protected
-		 * @return void
-		 */
-		protected function returndata() {
-			$end_time=microtime(true);
-			$processing_time=$end_time-$this->_start_time;
-			$this->data["processing_time"]=$processing_time;
-			if ($this->_render) {
-				$this->load->view("json",array("data"=>$this->data));
-			}
-		}
 	}
 
 /* End of file content.php */
--- 10layer/controllers/api/content_types.php
+++ 10layer/controllers/api/content_types.php
@@ -1,16 +1,13 @@
 <?php
+	require_once('10layer/system/TL_Api.php');
+	
 	/**
 	 * Content_Types class
 	 * 
 	 * @extends Controller
 	 */
-	class Content_types extends CI_Controller {
-		
-		protected $secure=false;
-		protected $_render=true;
-		private $_start_time=0;
-		public $vars=array();
-		public $data;
+	 
+	class Content_types extends TL_Api {
 		
 		/**
 		 * __construct function.
@@ -19,16 +16,7 @@
 		 * @return void
 		 */
 		public function __construct() {
-			$this->_start_time=microtime(true);
 			parent::__construct();
-			$this->data=array(
-				"error"=>false,
-				"timestamp"=>time(),
-				"msg"=>"",
-				"content"=>array()
-			);
-			$this->load->library("tlsecurity");
-			$this->tlsecurity->ignore_security();
 			$this->secure=$this->_check_secure();
 		}
 		
@@ -95,71 +83,6 @@
 			return true;
 		}
 		
-		/**
-		 * _check_secure function.
-		 * 
-		 * Returns true if we've sent a valid API key, else false
-		 *
-		 * @access private
-		 * @return boolean
-		 */
-		private function _check_secure() {
-			$api_key=$this->input->get_post("api_key");
-			$api_key=trim($api_key);
-			if (empty($api_key)) {
-				return false;
-			}
-			$comp_api_key=$this->config->item('api_key');
-			if (empty($comp_api_key)) {
-				return false;
-			}
-			if ($comp_api_key==$api_key) {
-				return true;
-			}
-			return false;
-		}
-		
-		/**
-		 * returndata function.
-		 * 
-		 * If $this->_render is true, print the results as json
-		 *
-		 * @access protected
-		 * @return void
-		 */
-		protected function returndata() {
-			$end_time=microtime(true);
-			$processing_time=$end_time-$this->_start_time;
-			$this->data["processing_time"]=$processing_time;
-			if ($this->_render) {
-				$this->load->view("json",array("data"=>$this->data));
-			}
-		}
-		
-		protected function json_check($filename="") {
-			switch (json_last_error()) {
-				case JSON_ERROR_NONE:
-					break;
-				case JSON_ERROR_DEPTH:
-					show_error($filename.' JSON error - Maximum stack depth exceeded');
-					break;
-				case JSON_ERROR_STATE_MISMATCH:
-					show_error($filename.' JSON error - Underflow or the modes mismatch');
-					break;
-				case JSON_ERROR_CTRL_CHAR:
-	            	show_error($filename.' JSON error - Unexpected control character found');
-					break;
-				case JSON_ERROR_SYNTAX:
-					show_error($filename.' JSON error - Syntax error, malformed JSON');
-					break;
-				case JSON_ERROR_UTF8:
-					show_error($filename.' JSON error - Malformed UTF-8 characters, possibly incorrectly encoded');
-					break;
-				default:
-					show_error($filename.' JSON error - Unknown error');
-				break;
-			}
-		}
 	}
 
 /* End of file .php */
