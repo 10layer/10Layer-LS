181f6600444b8fcc3bb63aecd19394a441c8473c
j-norwood-young <jason@10layer.com>
2012-11-25 18:47:30 +0200

    Add file upload feature

--- 10layer/controllers/api/content.php
+++ 10layer/controllers/api/content.php
@@ -212,6 +212,37 @@
 		}
 		
 		/**
+		 * upload function.
+		 * 
+		 * Upload a file to the system, returns with filename
+		 *
+		 * @access public
+		 * @return void
+		 */
+		public function upload() {
+			$raw = file_get_contents("php://input");
+			$dir = "/content/";
+			$parts=date("Y")."/".date("m")."/".date("d")."/";
+			
+			@mkdir(".".$dir.$parts, 0755, true);
+			if (!is_dir(".".$dir.$parts)) {
+				$this->show_error("."."$dir$parts is not a directory or doesn't exist");
+			}
+			if (empty($raw)) {
+				$this->show_error("File not received or empty");
+			}
+			if (isset($this->vars["filename"])) {
+				$filename = $this->vars["filename"];
+			} else {
+				$filename = md5($raw);
+			}
+			$this->data["message"]=".".$dir.$parts.$filename;
+			file_put_contents(".".$dir.$parts.$filename, $raw);
+			$this->data["content"]=array("filename"=>$filename, "full_name"=>$dir.$parts.$filename);
+			$this->returndata();
+		}
+		
+		/**
 		 * blank function.
 		 * 
 		 * Used to return data without doing a MongoDB lookup, for instance to get the meta data for a content type
