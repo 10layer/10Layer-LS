130327a6e2d28b3d55d068fbab57b8b02cd8d873
j-norwood-young <jason@10layer.com>
2012-11-28 12:05:55 +0200

    Publish API

---
+++ 10layer/controllers/api/publish.php
@@ -0,0 +1,50 @@
+<?php
+	require_once('10layer/system/TL_Api.php');
+	
+	/**
+	 * Publish class
+	 * 
+	 * @extends CI_Controller
+	 */
+	class Publish extends TL_Api {
+		
+		/**
+		 * __construct function.
+		 * 
+		 * @access public
+		 * @return void
+		 */
+		public function __construct() {
+			parent::__construct();
+		}
+		
+		public function save() {
+			$this->enforce_secure();
+			$x = 0;
+			//foreach($this->vars->zones as $zone) {
+			$this->mongo_db->where(array("_id"=>$this->vars->_id))->delete("published");
+			$this->mongo_db->insert("published", $this->vars);
+			$this->data["message"]="Section updated";
+			$this->returndata();
+			//}
+		}
+		
+		public function section($section_id) {
+			$result=$this->mongo_db->where(array("_id"=>$section_id))->get("published");
+			if (empty($result)) {
+				$this->show_error("No results found for $section_id");
+			}
+			$this->data["content"]=$result[0];
+			$this->returndata();
+		}
+		
+		public function zone($section_id, $zone_id) {
+			$section=array_pop($this->mongo_db->where(array("_id"=>$section_id))->get("published"));
+			if (empty($section)) {
+				$this->show_error("No results found for $section_id");
+			}
+			$this->data["content"] = $section->zones[$zone_id];
+			$this->returndata();
+		}
+	}
+?>
