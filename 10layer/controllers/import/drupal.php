7eda8a49655c6b9606a75fa30c740fa4aaf9784b
j-norwood-young <jason@10layer.com>
2012-12-04 11:31:15 +0200

    A Drupal importer

--- 10layer/controllers/import/drupal.php
+++ 10layer/controllers/import/drupal.php
@@ -16,10 +16,11 @@
 		 */
 		public function __construct() {
 			parent::__construct();
+			$this->load->library("datatransformations");
 		}
 		
 		public function index() {
-			$this->load->library("datatransformations");
+			
 			$dbuser = $this->input->get_post("user");
 			$dbpassword = $this->input->get_post("password");
 			$dbserver = $this->input->get_post("server");
@@ -55,14 +56,14 @@
 				foreach($nodes as $node) {
 					$revision = $this->drupal->order_by("timestamp DESC")->where("nid", $node->nid)->get("node_revisions")->row();
 					$data=new stdClass();
-					$data->title=$node->title;
+					$data->title = $node->title;
 					$data->content_type = $content_type->type;
 					$data->body = $revision->body;
-					$data->blurb = $revision->teaser;
-					$data->timestamp = $node->created;
-					$data->last_modified = $node->changed;
-					$data->start_date = $node->created;
-					$data->nid = $node->nid;
+					$data->blurb = trim(strip_tags($revision->teaser));
+					$data->timestamp = (Integer) $node->created;
+					$data->last_modified = (Integer) $node->changed;
+					$data->start_date = (Integer) $node->created;
+					$data->nid = (Integer) $node->nid;
 					$data->imported = true;
 					$data->workflow_status = 3;
 					$data->last_editor = "Administrator";
@@ -70,6 +71,97 @@
 					$this->mongo_db->insert("content", $data);
 					$x++;
 				}
+			}
+			print "Inserted $x items";
+		}
+		
+		public function files() {
+			$dbuser = $this->input->get_post("user");
+			$dbpassword = $this->input->get_post("password");
+			$dbserver = $this->input->get_post("server");
+			$dbdatabase = $this->input->get_post("database");
+			$website = $this->input->get_post("website");
+			$download = $this->input->get_post("download");
+			
+			if (empty($dbuser) || empty($dbpassword) || empty($dbserver) || empty($dbdatabase)) {
+				show_error("Fields required: user, password, server, database");
+			}
+			
+			$config['hostname'] = $dbserver;
+			$config['username'] = $dbuser;
+			$config['password'] = $dbpassword;
+			$config['database'] = $dbdatabase;
+			$config['dbdriver'] = "mysql";
+			$config['dbprefix'] = "";
+			$config['pconnect'] = FALSE;
+			$config['db_debug'] = TRUE;
+			$config['cache_on'] = FALSE;
+			$config['cachedir'] = "";
+			$config['char_set'] = "utf8";
+			$config['dbcollat'] = "utf8_general_ci";
+			
+			$this->drupal = $this->load->database($config, TRUE);
+			
+			if (!$this->check_drupal()) {
+				show_error("$dbdatabase does not appear to be a Drupal database");
+			}
+			
+			$files=$this->drupal->join("upload", "upload.fid=files.fid")->join("node", "upload.nid=node.nid")->join("node_revisions", "node_revisions.nid=upload.nid")->order_by("files.timestamp DESC")->get("files")->result();
+			$total_file_size = $this->drupal->select("SUM( filesize ) AS total_file_size")->get("files")->row()->total_file_size;
+			$tot = 0;
+			$max = 10;
+			$count = 0;
+			$x = 0;
+			foreach($files as $file) {	
+				$date=$file->timestamp;
+				$dir = "/content/";
+				$parts=date("Y", $date)."/".date("m", $date)."/".date("d", $date)."/";
+				
+				$filename = ".".$dir.$parts.$file->filename;
+				$source = $website."/".dirname($file->filepath)."/".rawurlencode(basename($file->filename));
+				if ($download) {
+					@mkdir(".".$dir.$parts, 0755, true);
+					if (!is_dir(".".$dir.$parts)) {
+						$this->show_error("."."$dir$parts is not a directory or doesn't exist");
+					}
+					if (!file_exists($filename)) {
+						$s=file_get_contents($source);
+						if (strlen($s) > 0) {
+							file_put_contents($filename, $s);				
+							print "$filename ";
+						} else {
+							print "<b>Error:</b> $filename ";
+						}
+						$count++;
+						
+					}
+					$fsize = filesize($filename);
+					$tot = $tot + $fsize;
+					$percent = round($tot / $total_file_size, 3);
+					print "$percent (".round($tot/1024)." of ".round($total_file_size/1024).")<br />";
+					flush();
+					if ($count > $max) {
+						die();
+					}
+				}
+				
+				$data = new stdClass();
+				$data->title = $file->title;
+				$data->content_type = "file";
+				$data->body = $file->body;
+				$data->file = $dir.$parts.$file->filename;
+				$data->blurb = trim(strip_tags($file->description));
+				$data->timestamp = (Integer) $file->created;
+				$data->last_modified = (Integer) $file->changed;
+				$data->start_date = (Integer) $file->created;
+				$data->nid = (Integer) $file->nid;
+				$data->fid = (Integer) $file->fid;
+				$data->imported = true;
+				$data->workflow_status = 3;
+				$data->last_editor = "Administrator";
+				$data->_id = $this->datatransformations->urlid($this, $data->title, false);
+				$this->mongo_db->insert("content", $data);
+				$x++;
 			}
 			print "Inserted $x items";
 		}
