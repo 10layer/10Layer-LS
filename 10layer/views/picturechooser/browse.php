3436850180b5a662504f181886e1d731acfb10fb
j-norwood-young <jason@10layer.com>
2013-01-28 01:08:09 +0200

    Fix up image browse

--- 10layer/views/picturechooser/browse.php
+++ 10layer/views/picturechooser/browse.php
@@ -1,214 +1,85 @@
-<?php
-	$contenttype="picture";
-	$tablename="pictures";
-	$name="picture";
-	$segs=$this->uri->segment_array();
-	
-	$qs=$_SERVER["QUERY_STRING"];
-	parse_str($qs, $parts);
-	
-?>
+<script src="/resources/knockout/knockout-2.2.0.js"></script>
 <script language="javascript">
-	document.domain=document.domain;
-	//console.log(window.top.opener.CKEDITOR);
-	$(function() {
+	var File = function(filename) {
+		var self = this;
 		
-		$("#picselect").ajaxComplete(function() {
-			cl.hide();
+		self.filename = ko.observable(filename);
+		self.filetype = ko.computed(function() {
+			return self.filename().substr(self.filename().lastIndexOf('.') + 1);
 		});
-		
-		$("#picselect").ajaxStart(function() {
-			cl.show();
+		self.imagename = ko.computed(function() {
+			return "/api/files/image?filename="+escape(self.filename().substr(8))+"&height=100&width=100&render=true";
 		});
-		
-		$("#picselect").load("/list/picture/0",function() {
-			search();
-			populateCreate();
+		self.returnimage = ko.computed(function() {
+			return "/api/files/image?filename="+escape(self.filename().substr(8))+"&height=300&width=300&render=true";
 		});
-			
-		
-		function search() {
-			var s=$("#picselect").find(".popupSearch").val();
-			$.getJSON("/list/picture/search/"+escape(s), function(data) {
-				renderPopup(data, "picselect", "<?= $contenttype ?>");
-			});
-		}
-		
-		$("#picselect").delegate(".popupPagination a", "click", function() {
-			var url=$(this).attr("href");
-			$.getJSON(url, function(data) {
-				renderPopup(data, "picselect", "<?= $contenttype ?>");
-			});
-			return false;
-		});
-		
-		$("#picselect").delegate(".singleselect", "click", function() {
-			$("#picselect").dialog("close");
-		});
-		
-		$("#picselect").delegate(".item-select", "click", function() {
-			
-			val=$(this).val();
-			title=$(this).parent().parent().find("content_title").html();
-			//test();
-			//opener.test();
-			window.opener.CKEDITOR.tools.callFunction('<?= $parts["CKEditorFuncNum"] ?>', "/workers/picture/display/"+val+"/scaleImage/300/300/true",  function() {
-				
-				// Get the reference to a dialog window.
-				var element, dialog = this.getDialog();
-				// Check if this is the Image dialog window.
-				if (dialog.getName() == 'image') {
-					// Get the reference to a text field that holds the "alt" attribute.
-					element = dialog.getContentElement( 'info', 'txtAlt' );
-					// Assign the new value.
-					if ( element )
-						element.setValue(title);
-				}
-			});
-			
-			window.close();
-			//location.href="/workers/picturechooser/edit/"+val;
-		});
-		
-		function updateResults_<?= $tablename."_".$name ?>() {
-			var id=$("#<?= $tablename."_".$name ?>_field").val();
-			if (id) {
-				$("#displayResults_<?= $tablename."_".$name ?>").empty();
-				$("#displayResults_<?= $tablename."_".$name ?>").load("/list/<?= $contenttype ?>/item/"+id);
-			}
-		}
-		
-		$("#picselect").delegate(".popupSearch", "click", function() {
-			if ($(this).val()=="Search...") {
-				$(this).val("");
-			}
-		});
-		
-		$("#picselect").delegate(".popupSearch", "keypress", function() {
-			clearTimeout($.data(this, 'timer'));
-			var wait = setTimeout(search, 500);
-			$(this).data('timer', wait);
-		});
-		
-		
-		
-		function populateCreate() {
-			$("#contentcreate").load(
-				"/create/fullview/picture"
-			);
-		}
-		
-		$("#contentcreate").delegate("#contentform","submit",function() {
-			$(this).ajaxSubmit({
-				iframe: true,
-				
-				beforeSubmit: function(a,f,o) { 
-					o.dataType = "json";
-				},
-				success: function(data) {
-					if (data.id) {
-						//console.log(data);
-						val=data.data.urlid;
-						title=data.data.title;
-						window.opener.CKEDITOR.tools.callFunction('<?= $parts["CKEditorFuncNum"] ?>', "/workers/picture/display/"+val+"/scaleImage/300/300/true",
-							function() {
-								// Get the reference to a dialog window.
-								var element, dialog = this.getDialog();
-								// Check if this is the Image dialog window.
-								if (dialog.getName() == 'image') {
-									// Get the reference to a text field that holds the "alt" attribute.
-									element = dialog.getContentElement( 'info', 'txtAlt' );
-									// Assign the new value.
-									if ( element )
-										element.setValue(title);
-								}
-							});
-						window.close();
-					}
-					if (data.error) {
-						//console.log(data);
-						$("#msgdialog").html("<div class='title'>"+data.msg+"</div> "+data.info);
-						$("#msgdialog").dialog({
-							modal: true,
-							buttons: {
-								Ok: function() {
-									$( this ).dialog( "close" );
-								}
-							}
-						});
-					}
-				}
-			});
-			return false;
-		});
-		
-		updateResults_<?= $tablename."_".$name ?>();
-		
-		$(".dropdown-header").click(function() {
-			$(this).next().slideToggle("fast", function() {
-				if ($(this).is(":visible")) {
-					//console.log("Visible");
-					$(this).prev().find(".ui-icon").removeClass("ui-icon-circle-triangle-e").addClass("ui-icon-circle-triangle-s");
-				} else {
-					//console.log("Invisible");
-					$(this).prev().find(".ui-icon").removeClass("ui-icon-circle-triangle-s").addClass("ui-icon-circle-triangle-e");
-				}
-			});
-		});
-	});
-	
-	function renderPopup(data, containerid, contenttype) {
-		$("#"+containerid).find(".popupResultsCount").html("Found "+data.count+" results");
-		if ($("#picselect").find(".popupSearch").val()) {
-			$("#"+containerid).find(".popupResultsClear").html("<a href='/list/<?= $contenttype ?>/search'>Clear</a>").click(function() {
-				$(".popupWorking").show();
-				$("#picselect").find(".popupSearch").val("");
-				$(this).html("");
-			});
-		}
-		$("#"+containerid).find(".popupPagination").html(data.pagination);
-		$("#"+containerid).find(".content_row").remove();
-		for(x=0;x<data.docs.length;x++) {
-		    doc=data.docs[x];
-		    
-		    $("#"+containerid)
-		    	.find("tr.template")
-		    	.clone()
-		    	.appendTo("."+contenttype+"-content")
-		    	.removeClass("template")
-		    	.addClass('content_row_'+x)
-		    	.addClass("content_row")
-		    	.find(".item-select").val(doc.id).end()
-		    	.find(".content_editlink")
-		    	.html("<a href='/workers/picturechooser/edit/"+doc.urlid+"'>Edit</a>")
-		    	.end()
-		    ;
-		    for (var i in doc) {
-		    	$("#"+containerid).find(".content_row_"+x).find("."+i).html(doc[i]);
-		    	$("#"+containerid).find(".content_row_"+x).find(".content_"+i).html(doc[i]);
-		    }
-		    $("#"+containerid).find(".content_row_"+x).find(".content_img").html(
-		    	"<img src='/workers/picture/display/"+doc.urlid+"/cropThumbnailImage/40/30' />"
-		    );
-		    if (x%2==0) {
-		    	$("#"+containerid).find(".content_row_"+x).addClass("odd");
-		    }
-		}
-		$(".popupWorking").hide();
 	}
 	
+	var Files = function() {
+		var self = this;
+		self.files = ko.observableArray();
+		self.filetypes = ko.observableArray();
+		self.activeFiles = ko.observableArray();
+		self.activeType = ko.observable("all");
+		self.count = ko.observable(0);
+		self.perpage = ko.observable(20);
+		self.pageindex = ko.observable(0);
+		
+		self.maxpages = ko.computed(function() {
+			return Math.ceil(self.count() / self.perpage())-1;
+		});
+		
+		self.pages = ko.computed(function() {
+			var pages = [];
+			for (var x=0; x<self.maxpages(); x++) {
+				pages.push({ page: (x+1) });
+			}
+			return pages;
+		});
+		
+		$.getJSON("/api/files/browse?limit="+self.perpage()+"&offset=0&api_key=<?= $this->config->item("api_key") ?>", function(data) {
+			self.filetypes(data.content.filetypes);
+			self.filetypes.unshift("all");
+			self.files(_.map(data.content.files, function(file) { return new File(file) }));
+			self.count(data.content.count);
+		});
+		
+		self.clickFileType = function() {
+			var type = String(this);
+			self.activeType(type);
+		}
+		
+		self.clickPage = function() {
+			var page = this.page - 1;
+			var offset = page * self.perpage();
+			self.pageindex(page);
+			$.getJSON("/api/files/browse?limit="+self.perpage()+"&offset="+offset+"&api_key=<?= $this->config->item("api_key") ?>", function(data) {
+				self.files(_.map(data.content.files, function(file) { return new File(file) }));
+			});
+		}
+		
+		self.clickImage = function() {
+			window.opener.CKEDITOR.tools.callFunction( <?= $_GET["CKEditorFuncNum"] ?>, this.returnimage() );
+			window.close();
+		};
+	}
+	
+	$(function() {
+		ko.applyBindings(new Files());
+	});
 </script>
 
-<div id="msgdialog"></div>
-<div class="dropdown-header" id="contentcreate_dropdown">
-	Create <span class="ui-icon ui-icon-circle-triangle-e" style="float: right"></span>
-</div>
-<div id="contentcreate" style="display: none">
-
-</div>
-<div id="displayResults_<?= $tablename."_".$name ?>" class="linkResults">
-	
-</div>
-<div id="picselect" >
-	
+<div class="filechooser">
+	<div class="pagination pagination-small">
+		<ul data-bind="foreach: pages">
+			<li data-bind="css: { active: $data.page === ($root.pageindex() + 1) }"><a href="#" data-bind="text: page, click: $parent.clickPage"></a></li>
+		</ul>
+	</div>
+	<ul class="thumbnails" data-bind="foreach: files">
+		<!-- ko if: ($parent.activeType() == filetype()) || ($parent.activeType() == "all") -->
+		<li>
+			<img src="" data-bind="attr: { src: imagename }, click: $root.clickImage" />
+		</li>
+		<!-- /ko -->
+	</ul>
 </div>
--- 10layer/views/templates/frameheader.php
+++ 10layer/views/templates/frameheader.php
@@ -1,38 +1,21 @@
 <!DOCTYPE html> 
 <html lang="en-gb" charset="UTF-8">
-<head> 
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" /> 
-	<?= $this->autoloader->javascript(); ?>
+	<title>10Layer CMS</title> 
+	<link rel="shortcut icon" href="/resources/images/favicon.ico" type="image/x-icon" /> 
+	<link rel="icon" href="/resources/images/favicon.ico" type="image/x-icon" /> 
+	
+	<link rel="home" href="<?= base_url() ?>" title="Home" />
+	
+	<link rel="stylesheet" href="/resources/bootstrap/css/bootstrap.min.css" type="text/css" media="screen, projection" charset="utf-8" />
+	
+	<link rel="stylesheet" href="/resources/chosen/chosen.css" type="text/css" media="screen, projection" charset="utf-8" />
+	<link rel="stylesheet" href="/resources/css/style.css" type="text/css" media="screen, projection" charset="utf-8" />
+	
+	<script type="text/javascript" src="/resources/jquery/jquery182.js"></script>
+	<script type="text/javascript" src="/resources/js/underscore-min.js"></script>
+	<script type="text/javascript" src="/resources/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/resources/chosen/chosen.jquery.js"></script>
+	<script type="text/javascript" src="/resources/js/10layer.js"></script>
 </head> 
 <body>
-	<div id="container">
-		<div id="canvasloader-container" class="wrapper"></div>
-		<script type="text/javascript" src="/tlresources/file/js/loader.js"></script>
-		<?php
-			if (!empty($msg)) {
-		?>
-		<div class="<?php if (!empty($msg["error"])) { print 'error '; } ?>message">
-			<?= $msg["msg"] ?>
-			<?php
-				if (!empty($msg["info"])) {
-					if (is_array($msg["info"])) {
-						foreach($msg["info"] as $info) {
-						?>
-						<div class="info">
-							<?= $info ?>
-						</div>
-						<?php
-						}
-					} else {
-					?>
-						<div class="info">
-							<?= $msg["info"] ?>
-						</div>
-					<?php
-					}
-				}
-			?>
-		</div>
-		<?php
-			}
-		?>
