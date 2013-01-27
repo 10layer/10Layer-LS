f0bb576535b608787736fc017387225e5f910e87
j-norwood-young <jason@10layer.com>
2013-01-28 01:08:51 +0200

    Some changes to login menu

--- 10layer/views/menus/main/login.php
+++ 10layer/views/menus/main/login.php
@@ -1,3 +1,5 @@
+<li class="menuitem"><a href="/home">Login</a></li>
 <li class="menuitem"><a href="http://wiki.10layer.com">10Layer Wiki</a></li>
 <li class="menuitem"><a href="http://www.10layer.com">10Layer.com</a></li>
+<li class="menuitem"><a href="https://github.com/10layer/10Layer-LS">10Layer on Github</a></li>
 <li class="menuitem"><a href="/user/retrieve_password">Forgot password</a></li>
