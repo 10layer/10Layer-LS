2a361d04ef528cef6729961d99d29202c0d8114c
Tumelo <tumelo@10layer.com>
2013-02-12 13:56:40 +0200

    fix the user setup

--- 10layer/views/setup/users.php
+++ 10layer/views/setup/users.php
@@ -3,6 +3,15 @@
 ?>
 <script src="/resources/knockout/knockout-2.2.0.js"></script>
 <script>
+	
+	$('.close_perms').live('click', function(){
+		$(this).parent().hide();
+	});
+	
+	$('.drop_activator').live('click', function(){
+		$(this).next().show();
+	});
+
 	var User = function(data) {
 		var self = this;
 		self.id = ko.observable(data._id);
@@ -11,14 +20,15 @@
 		});
 		self.name = ko.observable(data.name);
 		self.email = ko.observable(data.email);
-		self.password = ko.observable("");
+		self.password = ko.observable(data.password);
 		self.permission = ko.observable(data.permission);
 		self.isActive = ko.observable(data.isActive);
-		self.status = ko.observable(data.status);
-		self.statusIsActive = ko.computed({
-			read: function() { return (self.status() == "Active") },
-			write: function(data) { if (data) { self.status("Active"); } else { self.status("Suspended") }  }
-		});
+		//self.visible = false;
+		// self.status = ko.observable(data.status);
+		// self.statusIsActive = ko.computed({
+		// 	read: function() { return (self.status() == "Active") },
+		// 	write: function(data) { if (data) { self.status("Active"); } else { self.status("Suspended") }  }
+		// });
 		self.title = ko.computed(function() {
 			return (self.name().length > 0) ? self.name() : "New User";
 		});
@@ -27,11 +37,12 @@
 	var Users = function() {
 		var self = this;
 		self.users = ko.observableArray([]);
+
 		
 		$.getJSON("/api/users?api_key=<?= $this->config->item("api_key") ?>", function(data) {
 			mapped = _.map(data.content, function(item) { return new User(item) });
 			self.users(mapped);
-			self.users()[0].isActive(true);
+			//self.users()[0].visible(true);
 		});
 		
 		self.save = function() {
@@ -51,12 +62,14 @@
 		}
 		
 		self.add = function() {
-			var tmparr = self.users.removeAll();
-			_.each(tmparr, function(item) {
-				item.isActive = false;
-			});
-			self.users(tmparr);
-			self.users.push(new User( { id: "new", name: "", email: "", password: "", permission: "Editor", isActive: true, status: "Active" } ));
+			// var tmparr = self.users.removeAll();
+			// _.each(tmparr, function(item) {
+			// 	item.isActive = false;
+			// });
+			//self.users(tmparr);
+			var the_password = "<?= $this->tlsecurity->random_pass(8, 12) ?>";
+			console.log(the_password);
+			self.users.push(new User( { id: "new", name: "", email: "", password: the_password, permission: "Editor", isActive: true } ));
 			console.log(ko.toJSON(self.users));
 		};
 	}
@@ -67,6 +80,7 @@
 		$(".alert").on('click', '.close', function(e) {
 			e.preventDefault();
 			$(this).parent().hide();
+			
 		});
 	});
 </script>
@@ -85,41 +99,65 @@
 			<li><a href="/setup/security">Security</a></li>
 		</ul>
 	</div>
-	<div class="span7">
-		<p>Here you can add a user or edit existing users. You can set up as many users as you like, but be warned: a user can never be deleted, only disabled. All fields are required. Please save when you're done.</p>
-		<div class="tabbable tabs-left">
-			<ul class="nav nav-tabs">
-				<li><a href="#new" data-bind="click: add"><i class="icon-plus"></i></a></li>
-				<!-- ko foreach: users -->
-				<li data-bind="css: { active: isActive}"><a data-toggle="tab" data-bind="text: title, attr: { href: hash }"></a></li>
-				<!-- /ko -->
-			</ul>
-  			<div class="tab-content" >
-				<!-- ko foreach: users -->
-				<div data-bind="attr: { id: id }, css: { active: isActive }" class="tab-pane">
-					<label>Name</label>
-					<input type="text" name="name" placeholder="Joe Soap" data-bind="value: name">
-					<label>Email address</label>
-				    <input type="text" name="email" placeholder="admin@10layer.com" data-bind="value: email">
-				    <label>Password</label>
-				    <input type="password" name="password" placeholder="Leave blank to not change" data-bind="value: password">
-				    <span class="help-block">Random password: <?= $this->tlsecurity->random_pass(8, 12) ?></span>
-				    <label>Permissions</label>
-				    <label class="radio"><input type="radio" name="permission" value="Administrator"  data-bind="checked: permission, attr: { name: 'permission_' + id() }"> Administrator</label>
-				    <label class="radio"><input type="radio" name="permission" value="Editor" data-bind="checked: permission, attr: { name: 'permission_' + id() }"> Editor</label>
-				    <label class="radio"><input type="radio" name="permission" value="Viewer" data-bind="checked: permission, attr: { name: 'permission_' + id() }"> Viewer</label>
-				    <label class="checkbox"><input type="checkbox" name="status" value="1" data-bind="checked: statusIsActive" /> Account is Active</label>
+	<div class="span10">
+		<div class="row">
+			<div class='span7'>
+				<p>
+					Here you can add a user or edit existing users. 
+					You can set up as many users as you like, but be warned: a user can never be deleted, only disabled. 
+					All fields are required. Please save when you're done.
+				</p>
+			</div>
+			<div class='span3'>
+				<div class="btn-group pull-right">
+					<a href="#new" class="btn btn-success " data-bind="click: add"><i class="icon-plus"></i> Add New</a>
+				    <a class="btn btn-primary " href="#" data-bind="click: save"><i class="icon-thumbs-up icon-white"></i> Save users</a>
 				</div>
-				<!-- /ko -->
+
+				
 			</div>
 		</div>
+		<div class="span10">
+			<div id="save_success" class="alert alert-success fade in" style="display: none"><a class="close" href="#">&times;</a> Users updated</div>
+			<div id="save_fail" class="alert alert-error fade in" style="display: none"><a class="close" href="#">&times;</a> <h4>Failed to update Users</h4> <span id="fail_message"></span></div>
+		</div>
+		
+	
+				
+		<table class="table">
+			<tr>
+				<th>Name</th>
+				<th>Email</th>
+				<th>Password</th>
+				<th>Permissions</th>
+				<th class='centralise'>Account Active?</th>
+			</tr>
+			<!-- ko foreach: users -->
+			<tr>
+				<td><input type="text" name="name" placeholder="Joe Soap" class='grid_input' data-bind="value: name"></td>
+				<td><input type="text" name="email" placeholder="admin@10layer.com" class='grid_input' data-bind="value: email"></td>
+				<td><input type="password" name="password" placeholder="Leave blank to not change" class='grid_input' data-bind="value: password"></td>
+				<td>
+					<div class='permission_config'>
+						<label class='drop_activator'><span data-bind='text: permission'></span> <i class="icon-edit pull-right"></i> </label>
+						<div class='permissions_container'>
+							<span class='close_perms'>&times;</span>
+							<label class="radio"><input type="radio" name="permission" value="Administrator"  data-bind="checked: permission, attr: { name: 'permission_' + id() }"> Administrator</label>
+					    	<label class="radio"><input type="radio" name="permission" value="Editor" data-bind="checked: permission, attr: { name: 'permission_' + id() }"> Editor</label>
+					    	<label class="radio"><input type="radio" name="permission" value="Viewer" data-bind="checked: permission, attr: { name: 'permission_' + id() }"> Viewer</label>
+
+						</div>
+					</div>
+
+			    </td>
+				<td class='centralise'><input type="checkbox" name="status" value="" data-bind="checked: isActive" /></td>
+			</tr>
+			<!-- /ko -->
+		</table>
+
+
 	</div>
-	<div class="span3">
-		<a class="btn btn-large btn-primary" href="#" data-bind="click: save"><i class="icon-thumbs-up icon-white"></i> Save users</a>
-		<p />
-				<div id="save_success" class="alert alert-success fade in" style="display: none"><a class="close" href="#">&times;</a> Users updated</div>
-				<div id="save_fail" class="alert alert-error fade in" style="display: none"><a class="close" href="#">&times;</a> <h4>Failed to update Users</h4> <span id="fail_message"></span></div>
-	</div>
+	
 </div>
 </form>
 <?php
