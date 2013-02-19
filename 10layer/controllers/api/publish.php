8794cf6fd1815bfc8c11eb1887f6ca8e3ae4a8b5
j-norwood-young <jason@10layer.com>
2013-02-19 10:16:57 +0200

    Add ability to publish a single doc to a zone, and get a list of places a content type can publish

--- 10layer/controllers/api/publish.php
+++ 10layer/controllers/api/publish.php
@@ -69,18 +69,99 @@
 			$this->returndata();
 		}
 		
+		/**
+		 * publishable function.
+		 * 
+		 * Returns the possible sections and zones a content type can be published to
+		 *
+		 * @access public
+		 * @param mixed $content_type
+		 * @return void
+		 */
+		public function available_zones($content_type) {
+			$content_types=$this->model_collections->get_all();
+			$result = array();
+			foreach($content_types as $ct) {
+				$collections=$this->mongo_db->where(array("content_type"=>$ct->_id))->get("content");
+				foreach($collections as $collection) {
+					foreach($collection->zone as $zone) {
+						if (in_array($content_type, $zone["zone_content_types"])) {
+							$result[$collection->_id][] = array("section_id"=>$collection->_id, "zone_id"=>$zone["zone_urlid"], "section_name"=>$collection->title, "zone_name"=>$zone["zone_name"]);
+						}
+					}
+				}
+			}
+			$this->data["content"]=$result;
+			$this->returndata();
+		}
+		
 		public function document() {
 			$this->enforce_secure();
 			$section_id = $this->input->get_post("section_id");
 			if (empty($section_id)) {
 				$this->data["error"]=true;
-				$this->data["msg"][]="Var section_id required";
+				$this->data["msg"][]="section_id required";
 				$this->returndata();
 				return false;
 			}
-			$section=array_pop($this->mongo_db->get_where("content",array("_id"=>$section_id)));
-			
-			print_r($section);
+			$zone_id = $this->input->get_post("zone_id");
+			if (empty($section_id)) {
+				$this->data["error"]=true;
+				$this->data["msg"][]="zone_id required";
+				$this->returndata();
+				return false;
+			}
+			$id = $this->input->get_post("id");
+			if (empty($id)) {
+				$this->data["error"]=true;
+				$this->data["msg"][]="id required";
+				$this->returndata();
+				return false;
+			}
+			$section = array_pop($this->mongo_db->get_where("content",array("_id"=>$section_id)));
+			if (empty($section)) {
+				$this->data["error"]=true;
+				$this->data["msg"][]="Section $section_id not found";
+				$this->returndata();
+				return false;
+			}
+			if (!isset($section->zone[$zone_id])) {
+				$this->data["error"]=true;
+				$this->data["msg"][]="Zone $zone_id not found";
+				$this->returndata();
+				return false;
+			}
+			$zone = $section->zone[$zone_id];
+			$doc = array_pop($this->mongo_db->get_where("content",array("_id"=>$id)));
+			if (empty($doc)) {
+				$this->data["error"]=true;
+				$this->data["msg"][]="Document $id not found";
+				$this->returndata();
+				return false;
+			}
+			if (!in_array($doc->content_type, $zone["zone_content_types"])) {
+				$this->data["error"]=true;
+				$this->data["msg"][]="Content type {$doc->content_type} not allowed in zone $zone_id";
+				$this->returndata();
+				return false;
+			}
+			$published = array_pop($this->mongo_db->get_where("published",array("_id"=>$section_id)));
+			//Now we're ready to add this item to the zone
+			$newzone = $published->zones[$zone_id];
+			//Make sure it's not already in there somewhere
+			for($x=0; $x< sizeof($newzone); $x++) {
+				if ($newzone[$x]["_id"] == $id) {
+					unset($newzone[$x]);
+				}
+			}
+			//Add this item to the start of the zone
+			array_unshift($newzone, $doc);
+			//Make sure we don't have too many items
+			array_splice($newzone, $zone["zone_max_items"]);
+			//Set the new zone in Published
+			$published->zones[$zone_id] = $newzone;
+			$this->mongo_db->where(array("_id"=>$section_id))->update("published", array("zones"=>$published->zones));
+			print_r($published);
 		}
 	}
 ?>
