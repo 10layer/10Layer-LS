eaf7dfec09225224190cd4aec3da544db1a3ac5a
Jason Norwood-Young <jason@10layer.com>
2013-05-14 12:50:09 +0200

    Seed on random

--- 10layer/helpers/apikey_helper.php
+++ 10layer/helpers/apikey_helper.php
@@ -1,7 +1,7 @@
 <?php
 	function generate_api_key($len=8) {
-
+		mt_srand();
 		$today = date('YMD');
-		return md5($today.mt_rand(10000,90000).microtime());
+		return md5($today.mt_rand().microtime());
 	}
 ?>
