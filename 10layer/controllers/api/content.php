716788ea644273a41e4928a32f043e2a7b6903e4
j-norwood-young <jason@10layer.com>
2013-08-01 12:24:54 +0200

    Important fix - when saving on edit we were losing the content_type on published

--- 10layer/controllers/api/content.php
+++ 10layer/controllers/api/content.php
@@ -306,6 +306,7 @@
 						for($x = 0; $x < sizeof($zone); $x++) {
 							if ($zone[$x]["_id"] == $id) {
 								$zone[$x] = $data;
+								$zone[$x]->content_type = $content_type;
 								$zone[$x]->_id = $id;
 								$section->zones["$zonekey"] = $zone;
 								$update_result = $this->mongo_db->where(array("_id"=>$section_id))->upsert('published', $section);
