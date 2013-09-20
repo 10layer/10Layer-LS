8bcc9869b25c512406b5298f47f9ce0705c89eac
j-norwood-young <jason@10layer.com>
2013-09-20 13:16:29 +0200

    Much better image selector

--- 10layer/views/picturechooser/browse.php
+++ 10layer/views/picturechooser/browse.php
@@ -1,9 +1,26 @@
 <script src="/resources/knockout/knockout-2.2.1.js"></script>
 <script language="javascript">
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[[i]];
+};
+
 	var File = function(filename) {
 		var self = this;
 		
 		self.filename = ko.observable(filename);
+		self.width = ko.observable(600);
+		self.height = ko.observable(500);
+		self.bw = ko.observable(false);
+		self.bounding = ko.observable("bound");
+		self.greyscale = ko.observable(false);
+		self.quality = ko.observable(80);
+		self.format = ko.observable("jpg");
+		self.filesize = ko.observable(0);
+		self.isActive = ko.observable(false);
+
 		self.filetype = ko.computed(function() {
 			return self.filename().substr(self.filename().lastIndexOf('.') + 1);
 		});
@@ -13,6 +30,36 @@
 		self.returnimage = ko.computed(function() {
 			return "/api/files/image?filename="+escape(self.filename().substr(8))+"&height=300&width=300&render=true";
 		});
+		self.bigimage = ko.computed({
+			read: 
+				function() {
+					var boundstr = "";
+					if (self.bounding() == "bound") {
+						boundstr = "&bounding=true";
+					}
+					var greystr = "";
+					if (self.greyscale()) {
+						greystr = "&greyscale=true"
+					}
+					var url ="<?= base_url() ?>api/files/image?filename="+escape(self.filename().substr(8))+"&height="+self.height()+"&width="+self.width()+"&quality="+self.quality()+"&format="+self.format()+"&render=true"+boundstr+greystr;
+					if (self.isActive()) {
+						var req = $.ajax({
+							type: "HEAD",
+							url: url,
+							success: function () {
+								self.filesize(bytesToSize(req.getResponseHeader("Content-Length")));
+							}
+						});
+					}
+					return url;
+				}
+			
+		});
+
+		self.selectImage = function() {
+			window.opener.CKEDITOR.tools.callFunction( <?= $_GET["CKEditorFuncNum"] ?>, this.bigimage() );
+			window.close();
+		}
 	}
 	
 	var Files = function() {
@@ -24,6 +71,8 @@
 		self.count = ko.observable(0);
 		self.perpage = ko.observable(20);
 		self.pageindex = ko.observable(0);
+		self.showSingle = ko.observable(false);
+		self.file = ko.observable();
 		
 		self.maxpages = ko.computed(function() {
 			return Math.ceil(self.count() / self.perpage())-1;
@@ -59,9 +108,15 @@
 		}
 		
 		self.clickImage = function() {
-			window.opener.CKEDITOR.tools.callFunction( <?= $_GET["CKEditorFuncNum"] ?>, this.returnimage() );
-			window.close();
+			self.showSingle(true);
+			var f = new File(this.filename());
+			f.isActive(true);
+			self.file(f);
 		};
+
+		self.selectBrowse = function() {
+			self.showSingle(false);
+		}
 	}
 	
 	$(function() {
@@ -69,7 +124,7 @@
 	});
 </script>
 
-<div class="filechooser">
+<div data-bind="css: { hidden: showSingle }" class="filechooser container">
 	<div class="pagination pagination-small">
 		<ul data-bind="foreach: pages">
 			<li data-bind="css: { active: $data.page === ($root.pageindex() + 1) }"><a href="#" data-bind="text: page, click: $parent.clickPage"></a></li>
@@ -82,4 +137,54 @@
 		</li>
 		<!-- /ko -->
 	</ul>
+</div>
+
+<div data-bind="css: { hidden: !showSingle() }" class="filechooser container">
+	<div class="row" data-bind="with: file">
+		<div class="span6" >
+			<img data-bind="attr: { src: bigimage }" />
+		</div>
+		<div class="span4">
+			<h3>Select Options</h3>
+			<legend>Sizing</legend>
+			<label>Width</label>
+			<input type="text" data-bind="value: width" />
+
+			<label>Height</label>
+			<input type="text" data-bind="value: height" />
+
+			<label class="radio">
+				<input type="radio" name="bound" data-bind="checked: bounding" value="bound" /> Bound
+			</label>
+			<label class="radio">
+				<input type="radio" name="bound" data-bind="checked: bounding" value="crop" /> Crop
+			</label>
+
+			<legend>Output</legend>
+			<label class="radio">
+				<input type="radio" name="format" data-bind="checked: format" value="jpg" /> JPEG
+			</label>
+			<label class="radio">
+				<input type="radio" name="format" data-bind="checked: format" value="png" /> PNG
+			</label>
+			<label class="radio">
+				<input type="radio" name="format" data-bind="checked: format" value="gif" /> GIF
+			</label>
+			<label>Quality</label>
+			<input type="text" data-bind="value: quality" />
+
+			
+
+			<legend>Effects</legend>
+			<label class="checkbox"><input type="checkbox" value="bw" data-bind="checked: greyscale" /> Greyscale</label>
+		</div>
+		<div class="span2">
+			<div>File size: <span data-bind="text: filesize"></span></div>
+			<div><a href="#" class="btn btn-success" data-bind="click: selectImage">Use this image</a></div>
+			<label>Direct link to this image</label>
+			<div><input type="text" data-bind="value: bigimage" readonly="readonly" /></div>
+			<div><a href="#" class="btn btn-warning" data-bind="click: $parent.selectBrowse">Browse images</a></div>
+			
+		</div>
+	</div>
 </div>
