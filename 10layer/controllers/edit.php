641d2cf49ec13b95b35ebc75c02136f163752291
j-norwood-young <jason@10layer.com>
2013-11-27 22:26:07 +0200

    We no longer need a content type in the url - we can autodetect it

--- 10layer/controllers/edit.php
+++ 10layer/controllers/edit.php
@@ -20,9 +20,20 @@
 		
 		public function _remap() {
 			$this->load->library("socketio");
-			$type=$this->uri->rsegment(2);
-			$urlid=$this->uri->rsegment(3);
-			$data["content_type"]=$type;
+			if ($this->uri->total_rsegments() > 2) {
+				$content_type=$this->uri->rsegment(2);
+				$urlid=$this->uri->rsegment(3);
+				if ($content_type == "undefined") {
+					$content_type = $this->model_content->check_content_type($urlid);
+				}
+			} else {
+				$urlid = $this->uri->rsegment(2);
+				$content_type = $this->model_content->check_content_type($urlid);
+			}
+			if (empty($content_type)) {
+				show_error("Could not find content type");
+			}
+			$data["content_type"]=$content_type;
 			$data["urlid"]=$urlid;
 			$data["content_types"]=$this->model_content->get_content_types_list();
 			$this->load->view("content/edit",$data);
